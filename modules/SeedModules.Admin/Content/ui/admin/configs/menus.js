b94c411f1f0a5763c4667fa0c680f46e7a9bd7f9
fyl080801 <fyl080801@hotmail.com>
2018-02-28 09:40:14 +0800

    安装了jslint

--- modules/SeedModules.Admin/Content/ui/admin/configs/menus.js
+++ modules/SeedModules.Admin/Content/ui/admin/configs/menus.js
@@ -1,27 +1,28 @@
-define([
-    'SeedModules.Admin/ui/admin/configs'
-], function (configs) {
+define(['SeedModules.Admin/ui/admin/configs'], function(configs) {
     'use strict';
 
     configs.run([
         '$state',
         'SeedModules.Admin/ui/admin/configs/nav',
-        function ($state, nav) {
+        function($state, nav) {
             nav.add({
                 text: '系统管理',
                 icon: 'fa fa-cog fa-fw',
                 order: -1,
-                children: [{
-                    text: '用户管理',
-                    itemClicked: function (evt) {
-                        $state.go('admin.users');
+                children: [
+                    {
+                        text: '用户管理',
+                        itemClicked: function(evt) {
+                            $state.go('admin.users');
+                        }
+                    },
+                    {
+                        text: '角色管理',
+                        itemClicked: function(evt) {
+                            $state.go('admin.roles');
+                        }
                     }
-                }, {
-                    text: '角色管理',
-                    itemClicked: function (evt) {
-                        $state.go('admin.roles');
-                    }
-                }]
+                ]
             });
         }
     ]);
--- modules/SeedModules.Features/Content/ui/module.js
+++ modules/SeedModules.Features/Content/ui/module.js
@@ -1,37 +1,36 @@
-define([
-    'app/application'
-], function (application) {
-    'use strict';
+define(['app/application'], function(application) {
+    'use strict'
 
-    application.requires.push('modules.features');
+    application.requires.push('modules.features')
 
     return angular
-        .module('modules.features', [
-            'ui.router'
-        ])
+        .module('modules.features', ['ui.router'])
         .config([
             '$stateProvider',
-            function ($stateProvider) {
+            function($stateProvider) {
                 $stateProvider.state('admin.features', {
                     url: '/features',
                     title: '功能管理',
                     templateUrl: '/SeedModules.Features/ui/views/features.html',
-                    requires: ['SeedModules.AngularUI/ui/requires', 'SeedModules.Features/ui/requires']
-                });
+                    requires: [
+                        'SeedModules.AngularUI/ui/requires',
+                        'SeedModules.Features/ui/requires'
+                    ]
+                })
             }
         ])
         .run([
             '$state',
             'SeedModules.Admin/ui/admin/configs/nav',
-            function ($state, nav) {
+            function($state, nav) {
                 nav.add({
                     text: '功能管理',
                     icon: 'fa fa-cubes fa-fw',
                     order: 2,
-                    itemClicked: function (evt) {
-                        $state.go('admin.features');
+                    itemClicked: function(evt) {
+                        $state.go('admin.features')
                     }
-                });
+                })
             }
-        ]);
-});
+        ])
+})
