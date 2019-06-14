eb4d71b59f66ecba56eca69dc7362f937a4a2f03
fengyuanliang3 <fengyuanliang2@jd.com>
2019-06-14 23:50:07 +0800

    feat: react集成开发

--- src/SeedModules/SeedModules.Setup/Startup.cs
+++ src/SeedModules/SeedModules.Setup/Startup.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.DependencyInjection;
 using SeedCore.Modules;
 using System;
+using System.IO;
+using System.Reflection;
 
 namespace SeedModules.Setup
 {
@@ -21,6 +26,16 @@ namespace SeedModules.Setup
                 template: "",
                 defaults: new { controller = "Home", action = "Index" }
             );
+
+            app.UseSpa(spa =>
+            {
+                if (serviceProvider.GetService<IHostingEnvironment>().IsDevelopment())
+                {
+                    // react开发服务只能从相对路径加载
+                    spa.Options.SourcePath = "../SeedModules/SeedModules.Setup/wwwroot/ClientApp";
+                    spa.UseReactDevelopmentServer(npmScript: "start");
+                }
+            });
         }
     }
 }
